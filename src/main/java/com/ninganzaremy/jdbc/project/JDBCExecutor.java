c9d601289493151e64cc24419854ce0379ecfaac
Remy Ninganza <rninganz@asurite.asu.edu>
2022-02-02 14:46:40 -0700

    Implement a DAO

--- src/main/java/com/ninganzaremy/jdbc/project/JDBCExecutor.java
+++ src/main/java/com/ninganzaremy/jdbc/project/JDBCExecutor.java
@@ -9,26 +9,28 @@ public class JDBCExecutor {
                 "hplussport", "postgres", "password");
         try{
             Connection connection = dcm.getConnection();
-            CustomerDAO customerDAO = new CustomerDAO(connection);
-            Customer customer = new Customer();
-            customer.setFirstName("John");
-            customer.setLastName("Adams");
-            customer.setEmail("jadams.wh.gov");
-            customer.setAddress("1234 Main St");
-            customer.setCity("Arlington");
-            customer.setState("VA");
-            customer.setPhone("(555) 555-9845");
-            customer.setZipCode("01234");
-
-            Customer dbCustomer = customerDAO.create(customer);
-            System.out.println(dbCustomer);
-            dbCustomer = customerDAO.findById(dbCustomer.getId());
-            System.out.println(dbCustomer);
-            dbCustomer.setEmail("john.adams@wh.gov");
-            dbCustomer = customerDAO.update(dbCustomer);
-            System.out.println(dbCustomer);
-            customerDAO.delete(dbCustomer.getId());
-
+            OrderDAO orderDAO = new OrderDAO(connection);
+            Order order = orderDAO.findById(1000);
+            System.out.println(order);
+//            CustomerDAO customerDAO = new CustomerDAO(connection);
+//            Customer customer = new Customer();
+//            customer.setFirstName("John");
+//            customer.setLastName("Adams");
+//            customer.setEmail("jadams.wh.gov");
+//            customer.setAddress("1234 Main St");
+//            customer.setCity("Arlington");
+//            customer.setState("VA");
+//            customer.setPhone("(555) 555-9845");
+//            customer.setZipCode("01234");
+//
+//            Customer dbCustomer = customerDAO.create(customer);
+//            System.out.println(dbCustomer);
+//            dbCustomer = customerDAO.findById(dbCustomer.getId());
+//            System.out.println(dbCustomer);
+//            dbCustomer.setEmail("john.adams@wh.gov");
+//            dbCustomer = customerDAO.update(dbCustomer);
+//            System.out.println(dbCustomer);
+//            customerDAO.delete(dbCustomer.getId());
         }catch(SQLException e){
             e.printStackTrace();
         }
