834ff53f8f1b8a8e50e4e56aa6d4e7e5a91f3d23
Remy Ninganza <rninganz@asurite.asu.edu>
2022-02-02 14:25:57 -0700

    deleting data in JDBC

--- src/main/java/com/ninganzaremy/jdbc/project/CustomerDAO.java
+++ src/main/java/com/ninganzaremy/jdbc/project/CustomerDAO.java
@@ -20,6 +20,8 @@ public class CustomerDAO extends DataAccessObject<Customer> {
     private static final String UPDATE = "UPDATE customer SET first_name = ?, last_name=?, " +
             "email = ?, phone = ?, address = ?, city = ?, state = ?, zipcode = ? WHERE customer_id = ?";
 
+    private static final String DELETE = "DELETE FROM customer WHERE customer_id = ?";
+
 
     @Override
     public Customer findById(long id) {
@@ -94,6 +96,13 @@ public class CustomerDAO extends DataAccessObject<Customer> {
 
     @Override
     public void delete(long id) {
+        try(PreparedStatement statement = this.connection.prepareStatement(DELETE);){
+            statement.setLong(1, id);
+            statement.execute();
+        }catch (SQLException e){
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
 
     }
 }
